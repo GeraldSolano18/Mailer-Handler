4e5ce47bacc5e814139a24d1a5c06ecde0511c59
Gerald Solano <45377873+GeraldSolano18@users.noreply.github.com>
2021-10-24 01:51:37 -0600

    Develop (#1)
    * :hammer: server config
    
    * :alien: changes in index
    
    Co-authored-by: Gerald Solano <geraldmacias.gm@gmail.com>

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,43 @@ import nodemailer from "nodemailer";
 
 const router = Router();
 
+const userMail = "geraldmacias.gm@gmail.com";
+
 router.post("/send-email", (req, res) => {
   const data = req.body;
+
   const smtpTransport = nodemailer.createTransport({
-    service: "",
+    service: "Gmail",
+    port: 465,
+    auth: {
+      user: userMail,
+      pass: "amopatinar2019",
+    },
   });
-  console.log(req.body);
-  res.send("received");
+
+  const mailOptions = {
+    from: data.email,
+    to: userMail,
+    subject: `Message from ${data.name}`,
+    html: `
+    <h3>INFORMATION</h3>
+    <ul>
+    <li>Name: ${data.name}</li>
+    <li>Last Name: ${data.lastName}</li>
+    <li>Email: ${data.email}</li>
+    <li></li>
+
+    </ul>
+    `,
+  };
+  smtpTransport.sendMail(mailOptions, (err, res) => {
+    if (err) res.send(err);
+
+    res.send("send");
+    res.send("received");
+  });
+
+  smtpTransport.close();
 });
 
 export default router;
